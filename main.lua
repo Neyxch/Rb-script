c0ce3437faebfb86193d1e691695f11d07a9da78
Neyxch <ne.ycxh@gmail.com>
2026-03-23 16:47:30 +0200

    Create main.lua
    Added main teleport script for Roblox (Ctrl + Click teleport)

---
+++ main.lua
@@ -0,0 +1,29 @@
+local UIS = game:GetService("UserInputService")
+local StarterGui = game:GetService("StarterGui")
+
+local Player = game.Players.LocalPlayer
+local Mouse = Player:GetMouse()
+
+-- Уведомление
+StarterGui:SetCore("SendNotification", {
+    Title = "Script",
+    Text = "creator by: neyxch",
+    Duration = 3
+})
+-- End
+function GetCharacter()
+   return game.Players.LocalPlayer.Character
+end
+
+function Teleport(pos)
+   local Char = GetCharacter()
+   if Char then
+       Char:MoveTo(pos)
+   end
+end
+
+UIS.InputBegan:Connect(function(input)
+   if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
+       Teleport(Mouse.Hit.p)
+   end
+end)
