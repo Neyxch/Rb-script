f42f7fa7185decf1aacdf5966eaef7dea1fe8de4
Neyxch <ne.ycxh@gmail.com>
2026-03-29 14:51:18 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,6 @@
+---------------------------
+-- 🔹1: CLICK TP
+---------------------------
 local UIS = game:GetService("UserInputService")
 local StarterGui = game:GetService("StarterGui")
 
@@ -26,4 +29,21 @@ UIS.InputBegan:Connect(function(input)
    if input.UserInputType == Enum.UserInputType.MouseButton1 and UIS:IsKeyDown(Enum.KeyCode.LeftControl) then
        Teleport(Mouse.Hit.p)
    end
+end)
+---------------------------
+-- 🔹2: REJOIN GUI
+---------------------------
+local TeleportService = game:GetService("TeleportService")
+
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Parent = Player:WaitForChild("PlayerGui")
+
+local Button = Instance.new("TextButton")
+Button.Size = UDim2.new(0, 120, 0, 40)
+Button.Position = UDim2.new(0, 10, 0, 10)
+Button.Text = "REJOIN"
+Button.Parent = ScreenGui
+
+Button.MouseButton1Click:Connect(function()
+    TeleportService:Teleport(game.PlaceId, Player)
 end)
